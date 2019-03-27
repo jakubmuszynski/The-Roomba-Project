b0dc113add334991af00a2368698f7c56d2fb9c3
jakubmuszynski <j.muszynski@g.pl>
2019-03-27 12:08:56 +0100

    first commit

---
+++ create_autonomy/roomba/src/roomba.cpp
@@ -0,0 +1,18 @@
+#include <ros/ros.h>
+#include <iostream>
+//#include "ca_driver/ca_driver.h"
+
+int main(int argc, char** argv)
+{
+	std::cout << "Siemaneczkoooo" << std::endl;
+	ros::init(argc, argv, "test_node");
+
+	
+
+	//ros::start();
+	//ROS_INFO_STREAM("Siemaneczkoooo");
+	//ros::spin();
+	//ros::shutdown();
+
+	return 0;
+}
